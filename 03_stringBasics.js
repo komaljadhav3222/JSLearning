fa86b7869c180e46c5201993fa7bbecc2a2737e0
komaljadhav3222 <komaljadhav3222@gmail.com>
2023-09-25 11:55:50 +0530

    Updated string basics with length property
    Updated string basics with length property

--- 03_stringBasics.js
+++ 03_stringBasics.js
@@ -6,5 +6,7 @@ function stringBasics()
     var hobby2 = " Woolean Crochet,";
     var hobby3 = " Drawing";
     console.log(hobby1+hobby2+hobby3);
+    console.log("length of hobby1 : ",hobby1.length,"length of hobby2 : ",hobby2.length,"length of hobby3 : ",hobby3.length);
+    console.log("addition of length : ",hobby1.length+hobby2.length+hobby3.length);
 }
 stringBasics()
