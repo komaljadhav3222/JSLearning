f8df23311fef7baed596315a904c74667c2ba83c
komaljadhav3222 <komaljadhav3222@gmail.com>
2023-09-28 09:35:19 +0530

    updated result of Ternary operator assignment
    updated result of Ternary operator assignment

--- 05_OperatorTernaryC.js
+++ 05_OperatorTernaryC.js
@@ -1,20 +1,18 @@
 function maleMarriageEligibility(gender, age ,boyName){
 var result=gender=='Male'&&age>=21 
-? `Hey! ${boyName} Congratulations!, your age is ${age} and your gender is ${gender} so ,You are eligible for marriage`
-:`Hey! ${boyName} Sorry!,  your age is ${age} and your gender is ${gender} so ,You are not eligible for marriage`;
-// return "result is"+result;
-console.log(result);
+? console.log(`Hey! ${boyName} Congratulations!, your age is ${age} and your gender is ${gender} so ,You are eligible for marriage`)
+:console.log(`Hey! ${boyName} Sorry!,  your age is ${age} and your gender is ${gender} so ,You are not eligible for marriage`);
+return result;
 }
 maleMarriageEligibility("Male",25,"BillGates");
 maleMarriageEligibility("Male",17,"Stew Jobs");
 
 function femaleMarriageEligibility(gender, age ,girlName){
     var result=gender=='Female'&&age>=18 
-    ? `Hey! ${girlName} Congratulations!,  your age is ${age} and your gender is ${gender} so,You are eligible for marriage`
-    :`Hey! ${girlName} Sorry!, your age is ${age} and your gender is ${gender} so, You are not eligible for marriage`;
+    ? console.log(`Hey! ${girlName} Congratulations!,  your age is ${age} and your gender is ${gender} so,You are eligible for marriage`)
+    :console.log(`Hey! ${girlName} Sorry!, your age is ${age} and your gender is ${gender} so, You are not eligible for marriage`);
    
-   console.log(result);
-// return result;
+return result;
  }
     femaleMarriageEligibility("Female",16,"Jennifer");
     femaleMarriageEligibility("Female",27,"Malinda Gates");
