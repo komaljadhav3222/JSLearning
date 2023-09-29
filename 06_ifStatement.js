9dffef6459ae803a5bb57bb29e79e9c8dcbf157c
komaljadhav3222 <komaljadhav3222@gmail.com>
2023-09-29 10:02:19 +0530

    control flow - if else
    control flow - if else

---
+++ 06_ifStatement.js
@@ -0,0 +1,22 @@
+
+var girlAge=19;
+if(girlAge>=18)
+{
+console.log(`You should go inside office`);
+console.log(`Have you informed your Parents?`);
+console.log(`Signed ..Congrats!!`);
+}
+console.log(`you should not go inside office`);
+
+//if age is >=18 then allow for voting
+
+var JennyAge=17;
+if (JennyAge>=18) {
+    console.log(`Welcome`);
+    console.log(`you are eligible for vote`);
+    console.log(`let me take you fingerprints`);
+} else {
+    console.log(`You are not eligible for voting`);
+    console.log(`Try next time please`);
+}
+
