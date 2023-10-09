dedbc485604476906ac86bdafd7ffdccd35063c6
komaljadhav3222 <komaljadhav3222@gmail.com>
2023-10-09 09:06:14 +0530

    Objects in JS
    Objects in JS

---
+++ 09_objectAdv.js
@@ -0,0 +1,78 @@
+let person = {
+    name: "Jenny",
+    age: 22,
+    isMarried: true,
+    city: "Pune"
+}
+
+const keys = Object.keys(person);
+console.log(keys);
+
+const values = Object.values(person);
+console.log(values);
+const entry = Object.entries(person);// [ [], [], [], [] ]
+console.log(entry);
+
+//Traversing of an object
+console.log(`Traversing of an object using for of loop`);
+for (const element of entry) {
+    console.log(element[0], element[1]);
+}
+console.log(`Traversing of an object using for in loop`);
+for (const key in person) {
+console.log(key,person[key]);
+}
+
+const isNameAvailable ="name" in person;
+console.log(isNameAvailable);
+
+const isPinAvailable ="pin" in person;
+console.log(isPinAvailable);
+
+const personJenny = {
+    name: "Jenny",
+    age: 22,
+    isMarried: true,
+    city: "Pune"
+}
+personJenny.age = 25;
+console.log(personJenny);
+
+// personJenny = { }; Not allowed
+// personJenny = person; Not allowed
+Object.freeze(personJenny);
+personJenny.city = "Lucknow";
+console.log(personJenny);
+
+//Deep copy
+console.log(`Object.assign()`);
+
+let elon = {
+    name: "elon",
+    age: 54,
+    isMarried: true,
+    country: "USA"
+}
+
+const elonClone = Object.assign({},elon)
+console.log(elonClone);
+
+console.log(`Merging an objects`);
+let stew = {
+    name: "elon",
+    age: 54,
+    isMarried: true,
+    country: "USA"
+}
+
+const address= {
+    pin: 234567,
+    city: "Austin Texas"
+}
+Object.assign(stew, address);
+console.table(stew);
+
+// let array1=[1,2,3,4];
+// let array2 =[5,9,6,8,7];
+// array1=array1.concat(array2);
+// console.log(array1);
