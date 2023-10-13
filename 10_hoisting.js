a41cc24753c3c9a8fba34871f0e60979d45c5a1c
komaljadhav3222 <komaljadhav3222@gmail.com>
2023-10-13 08:39:53 +0530

    JSON handson
    JSON handson

---
+++ 10_hoisting.js
@@ -0,0 +1,24 @@
+/* Hoisting is a concept that enables us to extract values of variables and functions even before initializing/assigning value without getting errors and this happens during the 1st phase (memory creation phase) of the Execution Context.
+
+Features of Hoisting:
+
+In JavaScript, Hoisting is the default behavior of moving all the declarations at the top of the scope before code execution. Basically, it gives us an advantage that no matter where functions and variables are declared, they are moved to the top of their scope regardless of whether their scope is global or local. 
+It allows us to call functions before even writing them in our code. 
+Note: JavaScript only hoists declarations, not initializations. */
+
+console.log(personName);
+var personName="Elon Musk";
+
+console.log(personName);
+
+add();
+function add(){
+    console.log(10+20);
+}
+
+//mult(); --> can not hoist function expression
+var mult=function(){
+    const multiply=10*20;
+    console.log(multiply);
+}
+mult();
