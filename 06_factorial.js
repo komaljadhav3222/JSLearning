d06b2e6966c0ddd5c31998a7ac5ffaa48640ec16
komaljadhav3222 <komaljadhav3222@gmail.com>
2023-10-13 08:37:18 +0530

    updated prime no code
    passing array to function

--- 06_factorial.js
+++ 06_factorial.js
@@ -1,49 +1,65 @@
-console.log("factorial");
-function factorial(num){
-    let fact=1;
-for(let i=1;i<=num;i++){
-fact=fact*i;
-}
-console.log(fact);
-}
-factorial(4);
+// console.log("factorial");
+// function factorial(num){
+//     let fact=1;
+// for(let i=1;i<=num;i++){
+// fact=fact*i;
+// }
+// console.log(fact);
+// }
+// factorial(4);
 
-function isPrime(num){
-    var flag=0;
-for(let index=2;index<num;index++){
-   if(num%index==0){
-    flag=0;
-    break;
-   } 
-   else{
-    flag=1;
-   }
-}
-if(flag==1){
-    console.log(`"Given no.${num} is prime number"`);
-}
-else{
-    console.log(`"Given no.${num} is not prime number"`);
-}
-}
-isPrime(45);
-isPrime(7);
-isPrime(11);
-isPrime(13);
-isPrime(9);
+// function isPrime(num){
+//     var flag=0;
+// for(let index=2;index<num;index++){
+//    if(num%index==0){
+//     flag=0;
+//     break;
+//    } 
+//    else{
+//     flag=1;
+//    }
+// }
+// if(flag==1){
+//     console.log(`"Given no.${num} is prime number"`);
+// }
+// else{
+//     console.log(`"Given no.${num} is not prime number"`);
+// }
+// }
+// isPrime(45);
+// isPrime(7);
+// isPrime(11);
+// isPrime(13);
+// isPrime(9);
 
 
-console.log(`=================== Check whether the given Number is Prime or Not===================`);
-const isPrimeNo = num=> {
-  const boundary = Math. floor(Math.sqrt(num));
-  for (let index = 2; index <= boundary; index++) 
-  if (num % index===0) 
-  return false; 
-  return num >= 2;
-    
-  };
-  console.log(`The Number 11 is ${isPrimeNo(11)}`);
-  console.log(`The Number 23 is ${isPrimeNo(23)}`);
-  console.log(`The Number 31 is ${isPrimeNo(31)}`);
-  console.log(`The Number 44 is ${isPrimeNo(44)}`);
-  console.log(`The Number 56 is ${isPrimeNo(56)}`);
+// function isPrimeNumber(num){
+//     for (let index = 2; index < num; index++) {
+//         if (num%index==0) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// const result = isPrimeNumber(11);
+// console.log(`Is Number Prime: ${result}`);
+
+
+
+  const array =[11,3,6,10,43,29,50];
+  let result=0;
+  function isPrimeNumber(num){
+    for (let index = 2; index < num; index++) {
+        if (num%index==0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+     result=isPrimeNumber(element);
+     console.log(`Given number ${element} Is Prime: ${result}`);
+  }
+  
