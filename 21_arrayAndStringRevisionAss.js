dadb436ecd35720575fd829225f73f2311669a19
komaljadhav3222 <komaljadhav3222@gmail.com>
2023-10-18 14:09:21 +0530

    first and last element in upper case in given string
    first and last element in upper case in given string

--- 21_arrayAndStringRevisionAss.js
+++ 21_arrayAndStringRevisionAss.js
@@ -6,3 +6,26 @@ for (const iterator of set) {
    array.push(iterator);
 }
 console.log(`After removing duplicates - ${array}`);
+
+let str = "How are you mate";
+console.log(`Given String is  ${str}`);
+str=str.split(" ");
+// console.log(typeof str,str);
+let newString="";
+for (const iterator of str) {
+   // console.log(iterator);
+   for (const key in iterator) {
+      // console.log(key);
+      const char =iterator[key];
+      if(key==0 || key==iterator.length-1){
+         // console.log("iterator => ",iterator[key]);
+         
+         newString=newString+char.toUpperCase();
+      }
+      else{
+         newString=newString+char;
+      }
+   }
+   newString+=" ";
+}
+console.log(`Expected String is  ${newString}`);
