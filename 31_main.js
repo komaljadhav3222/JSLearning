bfcaa02c821afdcf32baf031794586646a520e99
komaljadhav3222 <komaljadhav3222@gmail.com>
2023-10-27 09:16:27 +0530

    event listener assignment
    event listener assignment

---
+++ 31_main.js
@@ -0,0 +1,13 @@
+
+
+
+const title=document.querySelector('#title');
+title.addEventListener('mouseover', ()=>{
+    title.style.color = 'blue';
+} );
+
+const list=document.querySelector('#list');
+const jira=document.querySelector('#jira');
+list.removeChild(jira);
+
+
